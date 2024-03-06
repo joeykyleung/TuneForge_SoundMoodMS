1e2560b2be17b1779892a7a5f4fafa5e51641351
vnp23 <vnp23@imperial.ac.uk>
2024-03-06 11:43:04 +0000

    overwriting blob storage

--- storage/azureStorage.py
+++ storage/azureStorage.py
@@ -11,6 +11,13 @@ container_name = environ.get('BLOB_CONTAINER_NAME', '')
 container_client = blob_service_client.get_container_client(container_name)
 
 
+def upload(local_path, blob_path):
+    # Upload the file
+    with open(local_path, "rb") as data:
+        container_client.upload_blob(name=blob_path, data=data,
+                                     overwrite=True)
+
+
 def download(local_path, blob_name):
     with open(local_path, "wb") as download_file:
         download_file.write(container_client.get_blob_client(
